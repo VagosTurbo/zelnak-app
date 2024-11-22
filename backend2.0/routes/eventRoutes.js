191d918da7680e49b1a1b0c65e63b438a56c5390
Marek Sucharda <m.sucharda@email.cz>
2024-11-22 18:58:55 +0100

    events - protect routes

--- backend2.0/routes/eventRoutes.js
+++ backend2.0/routes/eventRoutes.js
@@ -1,18 +1,25 @@
 /**
- * TODO: handle authentication for all routes
+ * Events DONE
  */
-
+import { Roles } from "../enums/roles.js"
 import { Router } from "express"
 import { getAllEvents, getEventById, createEvent, updateEvent, deleteEvent } from "../controllers/eventController.js"
 import { authenticateToken } from "../middleware/auth.js"
 
 const router = Router()
 
-router.get("/", getAllEvents)
-router.get("/:id", getEventById)
-// router.post("/", authenticateToken, createEvent)
-router.post("/", createEvent)
-router.put("/:id", updateEvent)
-router.delete("/:id", deleteEvent)
+const permissions = {
+    getAllEvents: [],
+    getEventById: [],
+    createEvent: [Roles.Admin, Roles.Farmer],
+    updateEvent: [Roles.Admin, Roles.Farmer],
+    deleteEvent: [Roles.Admin, Roles.Farmer],
+}
+
+router.get("/", authenticateToken(permissions.getAllEvents), getAllEvents)
+router.get("/:id", authenticateToken(permissions.getEventById), getEventById)
+router.post("/", authenticateToken(permissions.createEvent), createEvent)
+router.put("/:id", authenticateToken(permissions.updateEvent), updateEvent)
+router.delete("/:id", authenticateToken(permissions.deleteEvent), deleteEvent)
 
 export default router
