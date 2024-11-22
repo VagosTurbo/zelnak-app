6896a1aa2f88a701535a69a3327c9106221ba159
Marek Sucharda <m.sucharda@email.cz>
2024-11-22 18:56:10 +0100

    auth - authorized routes by role

--- backend2.0/middleware/auth.js
+++ backend2.0/middleware/auth.js
@@ -1,21 +1,28 @@
-// backend/middleware/auth.js
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken"
 
-const secretKey = process.env.JWT_SECRET;
+const secretKey = process.env.JWT_SECRET
 
-export const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+export const authenticateToken = (roles = []) => {
+    return (req, res, next) => {
+        const token = req.headers["authorization"]?.split(" ")[1]
 
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied' });
-    }
-
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: 'Invalid token' });
+        if (!token) {
+            return res.status(401).json({ message: "Access denied" })
         }
 
-        req.user = user; // Attach user information to the request object
-        next(); // Pass control to the next middleware function
-    });
-};
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.status(403).json({ message: "Invalid token" })
+            }
+
+            req.user = user // Attach user information to the request object
+
+            // Check if the user has one of the required roles
+            if (roles.length > 0 && !roles.includes(user.role)) {
+                return res.status(403).json({ message: "Forbidden: You do not have the required role" })
+            }
+
+            next() // Pass control to the next middleware function
+        })
+    }
+}
