16c4e28a95c20433bc2a83dc2c9fe859245f7aa3
VagosTurbo <semanco.boris@gmail.com>
2024-11-23 13:13:43 +0100

    merged successfully

--- backend2.0/models/category.js
+++ backend2.0/models/category.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // backend2.0/models/category.js
 import { poolPromise, sql } from '../config/database.js';
 
@@ -36,14 +35,4 @@ export const dbGetCategoryHierarchy = async (categoryId) => {
     }
 
     return hierarchy;
-=======
-import { poolPromise, sql } from '../config/database.js';
-
-export const dbGetCategoryById = async (id) => {
-    const pool = await poolPromise;
-    const result = await pool.request()
-        .input('id', sql.Int, id) // Binding the id parameter
-        .query('SELECT * FROM categories WHERE id = @id');
-    return result.recordset.length > 0 ? result.recordset[0] : null;
->>>>>>> origin/features/products
 };
