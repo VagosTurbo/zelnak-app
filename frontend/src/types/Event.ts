7c0e073e4eaad78a590c7e3bde93c2ceac49a60f
Marek Sucharda <m.sucharda@email.cz>
2024-11-23 19:18:08 +0100

    add all types

--- frontend/src/types/Event.ts
+++ frontend/src/types/Event.ts
@@ -1,13 +1,11 @@
-import { Category } from './Category'
 import { User } from './User'
 
-export interface Product {
+export interface Event {
     id: number
     name: string
-    price: number
     description: string
-    image: string
+    date: string
+    location: string
     user_id: User['id']
-    category_id: Category['id']
     created_at: string
 }
---
+++ frontend/src/types/Order.ts
@@ -0,0 +1,8 @@
+export interface Order {
+    id: number
+    seller_id: number
+    buyer_id: number
+    product_id: number
+    quantity: number
+    created_at: string
+}
