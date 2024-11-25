468e8f554f9f3a154d5459738e85a2e6b6fb6ea5
jakubp5 <jakub.pogadl@gmail.com>
2024-11-25 16:51:21 +0100

    cart price + approveorderitem fix

--- frontend/src/context/CartContext.tsx
+++ frontend/src/context/CartContext.tsx
@@ -1,39 +1,41 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react'
-import { Product } from '../types/Product'
+import React, { createContext, ReactNode, useContext, useState } from 'react'
 import { Cart } from '../types/Cart'
+import { Product } from '../types/Product'
 
 const CartContext = createContext<{
     cart: Cart
-    addProduct: (id: Product['id'], quantity: number, sellerId: number) => void
+    addProduct: (id: Product['id'], quantity: number, price: number, sellerId: number) => void
     removeProduct: (id: Product['id']) => void
+    updateProductQuantity: (id: Product['id'], quantityChange: number) => void
     clearCart: () => void
 }>({
     cart: { products: [] },
     addProduct: () => {},
     removeProduct: () => {},
+    updateProductQuantity: () => {},
     clearCart: () => {},
 })
 
 export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [cart, setCart] = useState<Cart>({ products: [] })
 
-    const addProduct = (id: Product['id'], quantity: number, sellerId: number) => {
+    const addProduct = (id: Product['id'], quantity: number, price: number, sellerId: number) => {
         setCart((prevCart) => {
-            const existingProduct = prevCart.products.find((p) => p.id === id);
+            const existingProduct = prevCart.products.find((p) => p.id === id)
             if (existingProduct) {
                 return {
                     ...prevCart,
                     products: prevCart.products.map((p) =>
                         p.id === id ? { ...p, quantity: p.quantity + quantity } : p
                     ),
-                };
+                }
             }
             return {
                 ...prevCart,
-                products: [...prevCart.products, { id, quantity, seller_id: sellerId }],
-            };
-        });
-    };
+                products: [...prevCart.products, { id, quantity, price, seller_id: sellerId }],
+            }
+        })
+    }
 
     const removeProduct = (id: Product['id']) => {
         setCart((prevCart) => ({
@@ -42,12 +44,22 @@ export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
         }))
     }
 
+    const updateProductQuantity = (id: Product['id'], quantityChange: number) => {
+        setCart((prevCart) => ({
+            ...prevCart,
+            products: prevCart.products.map((p) =>
+                p.id === id ? { ...p, quantity: p.quantity + quantityChange } : p
+            ),
+        }))
+    }
+
     const clearCart = () => {
         setCart({ products: [] })
     }
 
     return (
-        <CartContext.Provider value={{ cart, addProduct, removeProduct, clearCart }}>
+        <CartContext.Provider
+            value={{ cart, addProduct, removeProduct, updateProductQuantity, clearCart }}>
             {children}
         </CartContext.Provider>
     )
