0805c15c05f77e407fad2633ebb6f33542683766
VagosTurbo <semanco.boris@gmail.com>
2024-11-25 18:41:09 +0100

    order roles

--- backend2.0/routes/orderRoutes.js
+++ backend2.0/routes/orderRoutes.js
@@ -1,24 +1,88 @@
 import { Router } from "express";
-import { getAllOrders, getOrderById, createOrder, updateOrder, deleteOrder, getOrdersByUserId, getOrderItemsByOrderId, createOrderItem, updateOrderItem, deleteOrderItem, approveOrderItem, getOrderItemsBySellerId } from "../controllers/orderController.js";
+import {
+  getAllOrders,
+  getOrderById,
+  createOrder,
+  updateOrder,
+  deleteOrder,
+  getOrdersByUserId,
+  getOrderItemsByOrderId,
+  createOrderItem,
+  updateOrderItem,
+  deleteOrderItem,
+  approveOrderItem,
+  getOrderItemsBySellerId,
+} from "../controllers/orderController.js";
+import { Roles } from "../enums/roles.js";
+import { authenticateToken } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/", getAllOrders);
-router.get("/:id", getOrderById);
-router.post("/", createOrder);
-router.put("/:id", updateOrder);
-router.delete("/:id", deleteOrder);
-router.get("/user/:id", getOrdersByUserId);
+const permissions = {
+  getAllOrders: [Roles.Admin],
+  getOrderById: [],
+  createOrder: [Roles.Admin, Roles.Customer, Roles.Registered],
+  updateOrder: [Roles.Admin, Roles.Farmer, Roles.Customer],
+  deleteOrder: [Roles.Admin, Roles.Farmer],
+  getOrdersByUserId: [Roles.Admin, Roles.Farmer, Roles.Customer],
+  createOrderItem: [
+    Roles.Admin,
+    Roles.Farmer,
+    Roles.Registered,
+    Roles.Customer,
+  ],
+  updateOrderItem: [
+    Roles.Admin,
+    Roles.Farmer,
+    Roles.Registered,
+    Roles.Customer,
+  ],
+  deleteOrderItem: [Roles.Admin, Roles.Farmer, Roles.Customer],
+  getOrderItemsBySellerId: [Roles.Admin, Roles.Farmer],
+  approveOrderItem: [Roles.Admin, Roles.Farmer],
+};
 
-router.post("/:orderId/items", createOrderItem);
-router.get("/:orderId/items", getOrderItemsByOrderId);
-router.put("/items/:id", updateOrderItem);
-router.delete("/items/:id", deleteOrderItem);
+router.get("/", authenticateToken(permissions.getAllOrders), getAllOrders);
+router.get("/:id", authenticateToken(permissions.getOrderById), getOrderById);
+router.post("/", authenticateToken(permissions.createOrder), createOrder);
+router.put("/:id", authenticateToken(permissions.updateOrder), updateOrder);
+router.delete("/:id", authenticateToken(permissions.deleteOrder), deleteOrder);
+router.get(
+  "/user/:id",
+  authenticateToken(permissions.getOrdersByUserId),
+  getOrdersByUserId
+);
 
+router.post(
+  "/:orderId/items",
+  authenticateToken(permissions.createOrderItem),
+  createOrderItem
+);
+router.get(
+  "/:orderId/items",
+  authenticateToken(permissions.getOrderItemsByOrderId),
+  getOrderItemsByOrderId
+);
+router.put(
+  "/items/:id",
+  authenticateToken(permissions.updateOrderItem),
+  updateOrderItem
+);
+router.delete(
+  "/items/:id",
+  authenticateToken(permissions.deleteOrderItem),
+  deleteOrderItem
+);
 
-
-router.get("/:sellerId/items/", getOrderItemsBySellerId);
-router.put("/items/:id/approve", approveOrderItem);
-
+router.get(
+  "/:sellerId/items/",
+  authenticateToken(permissions.getOrderItemsBySellerId),
+  getOrderItemsBySellerId
+);
+router.put(
+  "/items/:id/approve",
+  authenticateToken(permissions.approveOrderItem),
+  approveOrderItem
+);
 
 export default router;
